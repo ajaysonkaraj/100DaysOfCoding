02b0ae688731914dbb44a3913b2b058214a1255d
Ajay <114383804+ajaysonkaraj@users.noreply.github.com>
2022-11-05 23:04:51 +0530

    Pattern Syntax Checker in Java

--- Day-16/PatternSyntaxChecker.java
+++ Day-16/PatternSyntaxChecker.java
@@ -7,12 +7,12 @@ public class Solution
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
 		while(testCases>0){
-			String pattern = in.nextLine();
+		   String pattern = in.nextLine();
                
-         try{
-            Pattern.compile(pattern);
-            System.out.println("Valid");
-        }
+                try{
+                     Pattern.compile(pattern);
+                     System.out.println("Valid");
+                   }
         catch(Exception e){
             System.out.println("Invalid");
         }
