ff7968ab0d2c582755d3bc61e715a07d68ddbfb2
Ajay <114383804+ajaysonkaraj@users.noreply.github.com>
2022-11-11 22:45:25 +0530

    BigInteger in java

---
+++ Day-22  BigInteger in java/Code.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        BigInteger a1 = sc.nextBigInteger();
+        BigInteger a2 = sc.nextBigInteger();
+        System.out.println(a1.add(a2));
+        System.out.println(a1.multiply(a2));
+        
+     }
+}
