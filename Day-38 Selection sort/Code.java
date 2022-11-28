e7d7d8dbaebf0503a0693a6fce20168d18212500
Ajay <114383804+ajaysonkaraj@users.noreply.github.com>
2022-11-28 23:23:47 +0530

    Sorting

---
+++ Day-38 Selection sort/Code.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Selection
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {5,7,2,1,-8,4};
+        System.out.println("\nBefore sorting \n"+ Arrays.toString(arr));
+        Scanner scan = new Scanner(System.in);
+        for(int i=0; i<arr.length; i++)
+        {
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[i] > arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("\nAfter Sorting \n"+Arrays.toString(arr));
+    }
+}
