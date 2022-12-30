5f53990035e5918fa823aaea99f998b461042109
Ajay <114383804+ajaysonkaraj@users.noreply.github.com>
2022-12-30 21:26:33 +0530

    File

---
+++ Day-62 Creating file/Code.java
@@ -0,0 +1,18 @@
+import java.io.File;
+ 
+
+class FileCreate {
+
+    public static void main(String[] args)
+
+    {
+ 
+
+        // File name specified
+
+        File obj = new File("myfile.txt");
+
+          System.out.println("File Created!");
+
+    }
+}
